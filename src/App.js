b8a391bf288dfe8092d969abf13e224026ee8da7
Carl2882 <109768651+Carl2882@users.noreply.github.com>
2023-04-26 01:58:15 +0000

    card layout set up/ API linkage

--- src/App.js
+++ src/App.js
@@ -1,41 +1,34 @@
-
+import React, {useEffect, useState} from 'react';
 import './input.css';
+import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
 
 function App() {
+  const [images,setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  // const [term, setTerm] = useState('');
+
+
+  useEffect(()=>{
+    fetch(`https://pixabay.com/api/?key=35774945-d3adb8480a29091e6c491628b&q=yellow+flowers&image_type=photo&pretty=true`)
+      .then(res => res.json())
+      .then(data => {
+        setImages(data.hits)
+        setIsLoading(false)
+      })
+      .catch(err => console.log(err))
+  });
+
+
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src='https://images.unsplash.com/photo-1679678691006-0ad24fecb769?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80' alt='' className='w-full'/>
-      <div className='px-6 py-4'>
-        <div className='font-bold text-orange-500 text-xl mb-2'>
-            Photo by Charles Tee
-        </div>
-        <ul>
-          <li>
-            <strong>Views:</strong>
-             2300
-          </li>
-          <li>
-            <strong>Downloads:</strong>
-             700
-          </li>
-          <li>
-            <strong>Likes:</strong>
-             300
-          </li>
-        </ul>
-      </div>
-      <div className='px-6 py-4'>
-        <span className='inline-block bg-gray-200 rounded-full px-3 py-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-          #tag1
-        </span>
-        <span className='inline-block bg-gray-200 rounded-full px-3 py-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-          #tag2
-        </span>
-        <span className='inline-block bg-gray-200 rounded-full px-3 py-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-          #tag3
-        </span>
-      </div>
-    </div>
+   <div className='container mx-auto'> 
+   <ImageSearch />
+      {isLoading ? <h1 className='text-6xl text-center mx-auto mt-32'>Loading...</h1> : <div className='grid grid-cols-3 gap-4'>
+        {images.map(image=>(
+          <ImageCard key={image.id} image={image}/>
+        ))}
+      </div>}
+   </div>
   );
 }
 
---
+++ src/components/ImageSearch.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../input.css'
+
+const ImageSearch = () =>{
+    return(
+        <div>
+       
+          </div>
+    )
+}
+
+export default ImageSearch;
