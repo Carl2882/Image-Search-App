bfff9ddd912d57a91da09cb98532a1923565591b
Carl2882 <109768651+Carl2882@users.noreply.github.com>
2023-04-26 00:34:16 +0000

    boiler plate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './input.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src='https://images.unsplash.com/photo-1679678691006-0ad24fecb769?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80' alt='' className='w-full'/>
+      <div className='px-6 py-4'>
+        <div className='font-bold text-orange-500 text-xl mb-2'>
+            Photo by Charles Tee
+        </div>
+        <ul>
+          <li>
+            <strong>Views:</strong>
+             2300
+          </li>
+          <li>
+            <strong>Downloads:</strong>
+             700
+          </li>
+          <li>
+            <strong>Likes:</strong>
+             300
+          </li>
+        </ul>
+      </div>
+      <div className='px-6 py-4'>
+        <span className='inline-block bg-gray-200 rounded-full px-3 py-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
+          #tag1
+        </span>
+        <span className='inline-block bg-gray-200 rounded-full px-3 py-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
+          #tag2
+        </span>
+        <span className='inline-block bg-gray-200 rounded-full px-3 py-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
+          #tag3
+        </span>
+      </div>
     </div>
   );
 }
