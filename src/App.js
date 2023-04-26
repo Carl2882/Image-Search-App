7a3ce0193107815177b299b91ac4b40eebf3decb
Carl2882 <109768651+Carl2882@users.noreply.github.com>
2023-04-26 14:28:00 +0000

    image search component implementation

--- src/App.js
+++ src/App.js
@@ -6,23 +6,26 @@ import ImageSearch from './components/ImageSearch';
 function App() {
   const [images,setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  // const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('');
 
 
   useEffect(()=>{
-    fetch(`https://pixabay.com/api/?key=35774945-d3adb8480a29091e6c491628b&q=yellow+flowers&image_type=photo&pretty=true`)
+    fetch(`https://pixabay.com/api/?key=35774945-d3adb8480a29091e6c491628b&q=${term}&image_type=photo&pretty=true`)
       .then(res => res.json())
       .then(data => {
-        setImages(data.hits)
-        setIsLoading(false)
+        setImages(data.hits);
+        setIsLoading(false);
       })
       .catch(err => console.log(err))
-  });
+  },[term]);
 
 
   return (
    <div className='container mx-auto'> 
-   <ImageSearch />
+   <ImageSearch searchText={(text)=>setTerm(text)}/>
+
+    {!isLoading && images.length === 0 && <h1 className='text-4xl text-center mx-auto mt-32'>No images found.</h1>}
+
       {isLoading ? <h1 className='text-6xl text-center mx-auto mt-32'>Loading...</h1> : <div className='grid grid-cols-3 gap-4'>
         {images.map(image=>(
           <ImageCard key={image.id} image={image}/>
